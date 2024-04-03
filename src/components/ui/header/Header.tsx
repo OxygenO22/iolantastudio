abf38ee946a07e1b1f741024572d51292815f8f7
OxygenO22 <bamaik@mail.ru>
2024-04-03 17:06:16 +0300

    Macked up icon and favicon

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -3,14 +3,19 @@ import { RouteButton } from '../buttos/routeButton/RouteButton';
 import { RouteButtonData } from '../buttos/routeButton/RouteButtonData';
 import { LogInIcon } from '../logInIcon/LogInIcon';
 import s from './Header.module.scss'
-import { Link } from 'react-router-dom';
-import { MyButton } from '../buttos/MyButton/MyButton';
 import { AuthButtons } from '../buttos/authButtons/AuthButtons';
+import Icon from '../../../pictures/icon/icon_3.jpg';
+import { useNavigate } from 'react-router-dom';
 
 export const Header = () => {
+  const nav = useNavigate();
   return (
     <div className={s.header__wrapper}>
-      <div className={s.icon__wrapper}>ICON</div>
+      <div className={s.icon__wrapper}>
+        <div className={s.icon__picture_wrapper} onClick={() => nav('/')}>
+          <img className={s.icon__picture} src={Icon} alt="Icon" />
+        </div>
+      </div>
       <div className={s.routebutton__wrapper}>
         {RouteButtonData.map((data) => (
           <RouteButton key={data.id} path={data.path} name={data.name} />
