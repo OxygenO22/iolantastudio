d2ab479ce4123d19efac4dfc090a4bbf12bb63da
OxygenO22 <bamaik@mail.ru>
2024-04-09 22:19:58 +0300

    Beginning creating the way how to get photos from instagram

--- src/components/pages/Galarypage/GalaryPage.tsx
+++ src/components/pages/Galarypage/GalaryPage.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import s from './GalaryPage.module.scss';
 import { PageTitle } from '../../ui/pageTitle/PageTitle';
 
+const url = '';
+
+function useInstagram() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        setPosts(data);
+      });
+  }, []);
+  return posts;
+}
+
 export const GalaryPage = () => {
+
+  const gramz = useInstagram();
   return (
     <div className={s.galary__wrapper}>
       <PageTitle title="Galary" />
+      <div>
+        {gramz.map((gram: any) => (
+          <a href={gram.url} key={gram.id}>
+            <img src={gram.thumbnail} alt={gram.caption} />
+          </a>
+        ))}
+      </div>
     </div>
   );
 }
